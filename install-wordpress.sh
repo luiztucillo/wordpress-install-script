b6e6dd4ef8e7cec5eb814e7f495fd946c62a5f9a
Luiz Tucillo <me@luiztucillo.com.br>
2020-02-26 16:09:25 -0300

    Create install-wordpress.sh

---
+++ install-wordpress.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+CURDIR=$(pwd)
+DOCKER_DATA_FOLDER=~/docker-data
+DB_NAME=wordpress
+DB_PASSWORD=1234qwer
+LOCALIZATION=pt_BR
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=linux;;
+    Darwin*)    machine=macos;;
+    CYGWIN*)    machine=windows;;
+    MINGW*)     machine=windows;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+while [ $# -gt 0 ]; do
+    if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+    fi
+    shift
+done
+
+if [ -z $dst ]; then
+    echo Argument --dst is required
+    exit 1
+fi
+
+if [ ! -z $docker_data_folder ]; then
+    DOCKER_DATA_FOLDER=$docker_data_folder
+fi
+
+if [ ! -z $db_name ]; then
+    DB_NAME=$db_name
+fi
+
+if [ ! -z $db_password ]; then
+    DB_PASSWORD=$db_password
+fi
+
+[ ! -d $dst ] && echo Path $dst does not exists && exit 1
+
+[ ! -d $DOCKER_DATA_FOLDER ] && echo Path $DOCKER_DATA_FOLDER does not exists && exit 1
+
+cd $dst
+rm -rf *
+rm -rf .docker && mkdir .docker
+cd .docker
+git clone git@github.com:luiztucillo/docker-php-apps.git ./
+echo -e "APPLICATION=wordpress\nPHP_VERSION=7.3\nDOMAIN=wordpress.local\nUSERNAME=$USER\nHOST_APP_PATH=../\nDOCKER_DATA_FOLDER=$DOCKER_DATA_FOLDER\nDB_PASSWORD=$DB_PASSWORD\nHOST_OS=$machine" > .env
+docker ps | awk '{print $1}' | xargs docker stop 2> /dev/null
+docker-compose up -d --build
+cd helpers/wordpress
+bash install-wpcli.sh
+cd ../../../
+
+while ! mysqladmin ping -uroot -p$DB_PASSWORD -h 127.0.0.1 --silent; do
+    sleep 1
+done
+
+echo -e "\nCreating database"
+docker exec wordpress_mysql mysql -uroot -p$DB_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+
+echo -e "\nDownloading"
+docker exec wordpress_php wp core download --locale=$LOCALIZATION
+
+echo -e "\nConfiguring wordpress"
+docker exec wordpress_php wp config create --dbname=$DB_NAME --dbuser=root --dbpass=$DB_PASSWORD --dbhost=mysql
+
+echo -e "\nInstalling wordpress"
+docker exec wordpress_php wp core install --url=wordpress.local --title=Example --admin_user=admin --admin_password=1234qwer --admin_email=info@example.com --admin_password=$DB_PASSWORD
+
+#echo -e "\nInstalling woocommerce"
+#docker exec wordpress_php wp plugin install woocommerce --activate
